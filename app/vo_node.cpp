d5a4594ea1439de087975ef2e4731cf74dd198b7
Tommaso <tommaso@example.com>
2024-03-23 22:57:17 +0100

    cant find libmyslam

--- app/vo_node.cpp
+++ app/vo_node.cpp
@@ -96,7 +96,7 @@ int main(int argc, char * argv[])
     if (argc > 1) {
         allArgs.assign(argv + 1, argv + argc);
     }
-    else throw std::runtime_error("Pass a config file.");
+    else throw std::runtime_error("[ERROR] Pass a config file.");
 
     std::string configPath(allArgs[0]);
 
